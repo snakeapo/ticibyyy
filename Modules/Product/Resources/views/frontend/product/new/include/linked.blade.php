aa9b7b966383ba280eb1403c1a1535827314fb87
snakeapo <xduke.apo@gmail.com>
2026-05-05 01:59:13 +0300

    fasda

---
+++ Modules/Product/Resources/views/frontend/product/new/include/linked.blade.php
@@ -0,0 +1,80 @@
+<section class="container pb-5 mb-1 mb-sm-2 mb-md-3 mb-lg-4 mb-xl-5">
+    <div class="bg-body-tertiary rounded-5 pt-5">
+        <h2 class="h3 text-center pb-2 py-lg-3">Cheaper together</h2>
+        <div class="row justify-content-center px-4 px-md-0">
+            <div class="col-md-10">
+                <div class="overflow-auto" data-simplebar data-simplebar-auto-hide="false">
+                    <div class="d-flex align-items-center justify-content-between pb-4 mb-2" style="min-width: 840px">
+
+                        <!-- Item -->
+                        <div class="w-100" style="max-width: 306px">
+                            <div class="form-check position-relative p-0 m-0 mb-3">
+                                <input type="checkbox" class="form-check-input position-absolute top-0 end-0 mt-3 me-3" id="iphone" checked disabled>
+                                <label for="iphone" class="d-flex justify-content-center bg-body rounded p-3">
+                        <span class="ratio d-block" style="max-width: 258px; --cz-aspect-ratio: calc(240 / 258 * 100%)">
+                          <img src="assets/img/shop/electronics/14.png" alt="iPhone 14">
+                        </span>
+                                </label>
+                            </div>
+                            <h3 class="mb-2">
+                                <a class="d-block fs-sm fw-medium animate-underline text-truncate" href="#!">
+                                    <span class="animate-target">Apple iPhone 14 128GB White</span>
+                                </a>
+                            </h3>
+                            <div class="h6 mb-0">$940.00</div>
+                        </div>
+
+                        <div class="ci-plus fs-4 mt-n5 mx-3 mx-lg-4"></div>
+
+                        <!-- Item -->
+                        <div class="w-100" style="max-width: 306px">
+                            <div class="form-check position-relative p-0 m-0 mb-3">
+                                <input type="checkbox" class="form-check-input position-absolute top-0 end-0 mt-3 me-3" id="airpods" checked>
+                                <label for="airpods" class="d-flex justify-content-center bg-body rounded p-3">
+                        <span class="ratio d-block" style="max-width: 258px; --cz-aspect-ratio: calc(240 / 258 * 100%)">
+                          <img src="assets/img/shop/electronics/06.png" alt="AirPods 2 Pro">
+                        </span>
+                                </label>
+                            </div>
+                            <h3 class="mb-2">
+                                <a class="d-block fs-sm fw-medium animate-underline text-truncate" href="#!">
+                                    <span class="animate-target">Headphones Apple AirPods 2 Pro</span>
+                                </a>
+                            </h3>
+                            <div class="d-flex align-items-center gap-2">
+                                <span class="badge bg-danger">-32%</span>
+                                <span class="h6 mb-0">$224.00</span>
+                                <del class="text-body-tertiary fs-xs">$330.00</del>
+                            </div>
+                        </div>
+
+                        <div class="ci-plus fs-4 mt-n5 mx-3 mx-lg-4"></div>
+
+                        <!-- Item -->
+                        <div class="w-100" style="max-width: 306px">
+                            <div class="form-check position-relative p-0 m-0 mb-3">
+                                <input type="checkbox" class="form-check-input position-absolute top-0 end-0 mt-3 me-3" id="charger">
+                                <label for="charger" class="d-flex justify-content-center bg-body rounded p-3">
+                        <span class="ratio d-block" style="max-width: 258px; --cz-aspect-ratio: calc(240 / 258 * 100%)">
+                          <img src="assets/img/shop/electronics/15.png" alt="Wireless charger">
+                        </span>
+                                </label>
+                            </div>
+                            <h3 class="mb-2">
+                                <a class="d-block fs-sm fw-medium animate-underline text-truncate" href="#!">
+                                    <span class="animate-target">Wireless charger for iPhone</span>
+                                </a>
+                            </h3>
+                            <div class="d-flex align-items-center gap-2">
+                                <span class="badge bg-danger">-48%</span>
+                                <span class="h6 mb-0">$26.00</span>
+                                <del class="text-body-tertiary fs-xs">$50.00</del>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
